8ef9871067d850a190175d1ebb3f11ef49d5d9f7
ER <106618521+siseruss@users.noreply.github.com>
2022-11-04 20:52:52 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,25 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    mySprite.say(myBoolean1 || myBoolean2)
+})
+let mySprite: Sprite = null
+let myBoolean1 = false
+let myBoolean2 = false
+myBoolean2 = false
+myBoolean1 = true
+scene.setBackgroundColor(13)
+mySprite = sprites.create(img`
+    . . 4 4 4 . . . . 4 4 4 . . . . 
+    . 4 5 5 5 e . . e 5 5 5 4 . . . 
+    4 5 5 5 5 5 e e 5 5 5 5 5 4 . . 
+    4 5 5 4 4 5 5 5 5 4 4 5 5 4 . . 
+    e 5 4 4 5 5 5 5 5 5 4 4 5 e . . 
+    . e e 5 5 5 5 5 5 5 5 e e . . . 
+    . . e 5 f 5 5 5 5 f 5 e . . . . 
+    . . f 5 5 5 4 4 5 5 5 f . . f f 
+    . . f 4 5 5 f f 5 5 6 f . f 5 f 
+    . . . f 6 6 6 6 6 6 4 4 f 5 5 f 
+    . . . f 4 5 5 5 5 5 5 4 4 5 f . 
+    . . . f 5 5 5 5 5 4 5 5 f f . . 
+    . . . f 5 f f f 5 f f 5 f . . . 
+    . . . f f . . f f . . f f . . . 
+    `, SpriteKind.Player)
